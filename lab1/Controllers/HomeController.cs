c66952fc76e4535287da8dd2ef76c70f232c6ee8
sheethal sebastian <sheethalsebastian2017@gmail.com>
2019-01-14 15:50:19 -0500

    end lesson 1

---
+++ lab1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is the application description page.";
+
+            return View("About-Us");
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+    }
+}
